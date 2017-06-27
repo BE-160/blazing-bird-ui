a8b34e0ddcdf567690c1cddbb4fd014bc8584c2b
Ankur Kushwaha <ankur.kushwaha@birdeye.com>
2017-06-27 09:15:51 +0530

    saga fixed

--- src/app/sagas/dataFetch.js
+++ src/app/sagas/dataFetch.js
@@ -1,26 +1,19 @@
-import { put, takeLatest } from "redux-saga/effects";
+import { put, call } from "redux-saga/effects";
 import fetch from "../utils/fetch";
+
+function fetchPost(url) {
+  return fetch(url).then((json) => json);
+}
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* fetchUser() {
   try {
     yield put({ type: "TIME_PENDING", user: user });
-    const user = yield fetch(`/posts`).then((json) => json);
+    const user = yield call(fetchPost, "/posts");
     yield put({ type: "TIME_FULFILLED", payload: user });
   } catch (e) {
     yield put({ type: "TIME_REJECTED", message: e.message });
   }
 }
 
-/*
-  Alternatively you may use takeLatest.
-
-  Does not allow concurrent fetches of user. If "USER_FETCH_REQUESTED" gets
-  dispatched while a fetch is already pending, that pending fetch is cancelled
-  and only the latest one will be run.
-*/
-function* mySaga() {
-  yield takeLatest("DATA_FETCH_REQUESTED", fetchUser);
-}
-
-export default mySaga;
+export default fetchUser;
--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,2 +1,7 @@
-import mysaga from "./dataFetch";
-export default mysaga;
+import { takeLatest } from "redux-saga/effects/";
+import dataFetch from "./dataFetch";
+
+// use them in parallel
+export default function* rootSaga() {
+  yield takeLatest("DATA_FETCH_REQUESTED", dataFetch);
+}
