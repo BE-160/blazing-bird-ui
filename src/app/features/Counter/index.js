f57e2f897b24408e3f3cc6418674854182d225d6
Ankur Kushwaha <ankur.kushwaha@birdeye.com>
2017-06-28 15:08:07 +0530

    readme updated

--- src/app/features/Counter/index.js
+++ src/app/features/Counter/index.js
@@ -1,3 +1,25 @@
-import CounterView from "./CounterContainer";
+import { connect } from "react-redux";
+import { increment } from "./actions";
 
-export default CounterView;
+/*  This is a container component. Notice it does not contain any JSX,
+    nor does it import React. This component is **only** responsible for
+    wiring in the actions and state necessary to render a presentational
+    component - in this case, the counter:   */
+
+import Counter from "./Counter";
+
+/*  Object of action creators (can also be function that returns object).
+    Keys will be passed as props to presentational components. Here we are
+    implementing our wrapper around increment; the component doesn't care   */
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => {
+    dispatch(increment(1));
+  }
+});
+
+const mapStateToProps = (state) => ({
+  counter: state.counterReducer.counter
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
